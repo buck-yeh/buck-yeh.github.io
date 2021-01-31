e5393efff9889e98c9049fff81fc87bbf2d16dd7
buck-yeh <buck.yeh@gmail.com>
2021-01-31 17:03:55 +0800

    Change bkcolor

--- bux/html/search/enumvalues_6.js
+++ bux/html/search/enumvalues_6.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['uie_5feof_978',['UIE_EOF',['../namespacebux.html#a2ca2cd5a25f5e2ed19eb5051ffeb9818a253de3ab522fb068fb80c78d360455a8',1,'bux']]],
-  ['uie_5fillformed_5funicode_979',['UIE_ILLFORMED_UNICODE',['../namespacebux.html#a2ca2cd5a25f5e2ed19eb5051ffeb9818a151e5f562a5c4230916433086b5c6c4d',1,'bux']]],
-  ['uie_5fincomplete_5funicode_980',['UIE_INCOMPLETE_UNICODE',['../namespacebux.html#a2ca2cd5a25f5e2ed19eb5051ffeb9818a421b8fabceb31d3edbe2ba75ebdf22af',1,'bux']]],
-  ['uie_5finternal_981',['UIE_INTERNAL',['../namespacebux.html#a2ca2cd5a25f5e2ed19eb5051ffeb9818aa020812ac6369f196f93ab07b5468602',1,'bux']]],
-  ['uie_5fno_5funicode_5ftranslation_982',['UIE_NO_UNICODE_TRANSLATION',['../namespacebux.html#a2ca2cd5a25f5e2ed19eb5051ffeb9818abb150365b152621578d070dc30969b66',1,'bux']]]
+  ['uie_5feof_978',['UIE_EOF',['../namespacebux.html#a69e0f18cf19940d58cf4c0a4853ef98da253de3ab522fb068fb80c78d360455a8',1,'bux']]],
+  ['uie_5fillformed_5funicode_979',['UIE_ILLFORMED_UNICODE',['../namespacebux.html#a69e0f18cf19940d58cf4c0a4853ef98da151e5f562a5c4230916433086b5c6c4d',1,'bux']]],
+  ['uie_5fincomplete_5funicode_980',['UIE_INCOMPLETE_UNICODE',['../namespacebux.html#a69e0f18cf19940d58cf4c0a4853ef98da421b8fabceb31d3edbe2ba75ebdf22af',1,'bux']]],
+  ['uie_5finternal_981',['UIE_INTERNAL',['../namespacebux.html#a69e0f18cf19940d58cf4c0a4853ef98daa020812ac6369f196f93ab07b5468602',1,'bux']]],
+  ['uie_5fno_5funicode_5ftranslation_982',['UIE_NO_UNICODE_TRANSLATION',['../namespacebux.html#a69e0f18cf19940d58cf4c0a4853ef98dabb150365b152621578d070dc30969b66',1,'bux']]]
 ];
