be4c2bef0feed998bee93e951cc50c163f22e7fc
buck-yeh <buck.yeh@gmail.com>
2022-05-22 14:37:33 +0800

    Update for `bux` v1.6.4

--- bux/html/search/files_8.js
+++ bux/html/search/files_8.js
@@ -2,8 +2,7 @@ var searchData=
 [
   ['paralog_2ecpp_0',['ParaLog.cpp',['../ParaLog_8cpp.html',1,'']]],
   ['paralog_2eh_1',['ParaLog.h',['../ParaLog_8h.html',1,'']]],
-  ['parautil_2eh_2',['ParaUtil.h',['../ParaUtil_8h.html',1,'']]],
-  ['parserbase_2ecpp_3',['ParserBase.cpp',['../ParserBase_8cpp.html',1,'']]],
-  ['parserbase_2eh_4',['ParserBase.h',['../ParserBase_8h.html',1,'']]],
-  ['partialordering_2eh_5',['PartialOrdering.h',['../PartialOrdering_8h.html',1,'']]]
+  ['parserbase_2ecpp_2',['ParserBase.cpp',['../ParserBase_8cpp.html',1,'']]],
+  ['parserbase_2eh_3',['ParserBase.h',['../ParserBase_8h.html',1,'']]],
+  ['partialordering_2eh_4',['PartialOrdering.h',['../PartialOrdering_8h.html',1,'']]]
 ];
