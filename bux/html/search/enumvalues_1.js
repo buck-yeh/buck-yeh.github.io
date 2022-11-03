b082983b83fe15ffeaccb958f3a10859247ddafa
buck-yeh <buck.yeh@gmail.com>
2022-11-03 19:03:54 +0800

    Update with advent of Doxygen v1.9.5

--- bux/html/search/enumvalues_1.js
+++ bux/html/search/enumvalues_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['fa_5foptional_0',['FA_OPTIONAL',['../namespacebux.html#a8329f44bd154d35fad059b53aed20ee5a0895efb4736305ddf2b94c603dc7224e',1,'bux']]],
-  ['fa_5frepeatable_1',['FA_REPEATABLE',['../namespacebux.html#a8329f44bd154d35fad059b53aed20ee5ae1108f4fa5a60eefac36865676f06cfd',1,'bux']]]
+  ['fa_5foptional_0',['FA_OPTIONAL',['../namespacebux.html#ab46181ee6aedcd6a29e2fe7515200f89a0895efb4736305ddf2b94c603dc7224e',1,'bux']]],
+  ['fa_5frepeatable_1',['FA_REPEATABLE',['../namespacebux.html#ab46181ee6aedcd6a29e2fe7515200f89ae1108f4fa5a60eefac36865676f06cfd',1,'bux']]]
 ];
