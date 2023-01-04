ae7dd5bd39fdb08c8ea0b0d4dbcbe74eb56e85f7
buck-yeh <buck.yeh@gmail.com>
2023-01-04 23:01:37 +0800

    Update with advent of Doxygen v1.9.6

--- bux/html/search/defines_2.js
+++ bux/html/search/defines_2.js
@@ -4,7 +4,7 @@ var searchData=
   ['def_5flogger_5fcerr_1',['DEF_LOGGER_CERR',['../Logger_8h.html#a956181da862320e74e6b9f778acd2843',1,'Logger.h']]],
   ['def_5flogger_5fcout_2',['DEF_LOGGER_COUT',['../Logger_8h.html#aac5fc4a7825758d5053fd9e00caef8fd',1,'Logger.h']]],
   ['def_5flogger_5ffile_3',['DEF_LOGGER_FILE',['../Logger_8h.html#ac83c2d55410c51e530560b82a1969f75',1,'Logger.h']]],
-  ['def_5flogger_5ffiles_4',['DEF_LOGGER_FILES',['../Logger_8h.html#afda064e22883755fe399271f283b68d6',1,'Logger.h']]],
+  ['def_5flogger_5ffiles_4',['DEF_LOGGER_FILES',['../Logger_8h.html#ab60cea9940998f781604c6dbad0ad44f',1,'Logger.h']]],
   ['def_5flogger_5fhead_5f_5',['DEF_LOGGER_HEAD_',['../Logger_8h.html#a14ef85e61da18a9e0d32f3780ac38682',1,'Logger.h']]],
   ['def_5flogger_5fostream_6',['DEF_LOGGER_OSTREAM',['../Logger_8h.html#a205474bfcfd706322fc5825f43e94eb9',1,'Logger.h']]],
   ['def_5flogger_5ftail_5f_7',['DEF_LOGGER_TAIL_',['../Logger_8h.html#a34f44d967ad385a8866c09ccdd49c24e',1,'Logger.h']]],
