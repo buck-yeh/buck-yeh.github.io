4f78e9c9a28447e0a70d57120de6b8a6a8b881c3
buck-yeh <buck.yeh@gmail.com>
2024-10-26 21:29:20 +0800

    Update with `bux` v1.9.0

--- bux/html/search/enumvalues_5.js
+++ bux/html/search/enumvalues_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['tid_5feof_0',['TID_EOF',['../namespacebux.html#a977b8db8ae52b0bab404e1a710ebf54daaeb340667d66cada60681afc4465bcbc',1,'bux']]],
-  ['tokengen_5flb_1',['TOKENGEN_LB',['../namespacebux.html#a977b8db8ae52b0bab404e1a710ebf54da4e8095430f859530ff4b9dc9e3894a38',1,'bux']]]
+  ['tid_5feof_0',['TID_EOF',['../namespacebux.html#aa1c6f7bc99af72ad9241102b51933ec8aaeb340667d66cada60681afc4465bcbc',1,'bux']]],
+  ['tokengen_5flb_1',['TOKENGEN_LB',['../namespacebux.html#aa1c6f7bc99af72ad9241102b51933ec8a4e8095430f859530ff4b9dc9e3894a38',1,'bux']]]
 ];
--- bux/html/search/files_2.js
+++ bux/html/search/files_2.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['ezargs_2ecpp_0',['EZArgs.cpp',['../EZArgs_8cpp.html',1,'']]],
-  ['ezargs_2eh_1',['EZArgs.h',['../EZArgs_8h.html',1,'']]]
+  ['ezargs_2eh_1',['EZArgs.h',['../EZArgs_8h.html',1,'']]],
+  ['ezscape_2ecpp_2',['EZScape.cpp',['../EZScape_8cpp.html',1,'']]],
+  ['ezscape_2eh_3',['EZScape.h',['../EZScape_8h.html',1,'']]]
 ];
